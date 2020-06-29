5be7d5032693116e3541c0f1f18f053c01cb3b80
Maxime Desbieys <desbieysmaxime@gmail.com>
2020-06-29 14:48:12 +0200

    feat(dto) ajout constructeur, getter, setter

--- java/src/main/java/com/codurance/training/tasks/Project.java
+++ java/src/main/java/com/codurance/training/tasks/Project.java
@@ -1,4 +1,29 @@
 package com.codurance.training.tasks;
 
+import java.util.List;
+
 public class Project {
+    private String name;
+    private List<Task> tasks;
+
+    public Project(String name, List<Task> tasks) {
+        this.name = name;
+        this.tasks = tasks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
